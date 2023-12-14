58d00ce3e6f1f13e8142d93e1fac8311fcd28907
connorcarnes <connorcarnes44@gmail.com>
2023-12-14 15:21:53 -0600

    add tests for Remove-CFZoneRecord, Set-CFZoneRecord, Set-CloudflareSession, Test-CloudflareSession

---
+++ src/Tests/Unit/Public/Remove-CFZoneRecord.Tests.ps1
@@ -0,0 +1,57 @@
+﻿#-------------------------------------------------------------------------
+Set-Location -Path $PSScriptRoot
+#-------------------------------------------------------------------------
+$ModuleName = 'pwshCloudflare'
+$PathToManifest = [System.IO.Path]::Combine('..', '..', '..', $ModuleName, "$ModuleName.psd1")
+#-------------------------------------------------------------------------
+if (Get-Module -Name $ModuleName -ErrorAction 'SilentlyContinue') {
+    #if the module is already in memory, remove it
+    Remove-Module -Name $ModuleName -Force
+}
+Import-Module $PathToManifest -Force
+#-------------------------------------------------------------------------
+
+InModuleScope 'pwshCloudflare' {
+    Describe 'Remove-CFZoneRecord Function Tests' -Tag Unit {
+        BeforeAll {
+            $WarningPreference = 'SilentlyContinue'
+            $ErrorActionPreference = 'SilentlyContinue'
+            $script:cfBaseApiUrl = 'https://api.cloudflare.com/client/v4'
+            $Script:cfZoneLookupTable = @{'example.com' = '12345' }
+            Mock Invoke-CFRestMethod { return @{ result = @{'id' = '12345' } } }
+        }
+        BeforeEach {
+            $script:cfSession = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+        }
+        Context 'Error' {
+            It 'should throw an error if Cloudflare session is not found' {
+                $ZoneName = 'example.com'
+                $script:cfSession = $null
+                { Remove-CFZoneRecord -ZoneName $ZoneName -RecordId '12345' } | Should -Throw
+            }
+        }
+        Context 'Success' {
+            It 'Calls Invoke-CFRestMethod with correct parameters for ZoneName' {
+                Remove-CFZoneRecord -ZoneName 'example.com' -RecordId '12345'
+                Assert-MockCalled Invoke-CFRestMethod -Exactly 1 -Scope It -ParameterFilter {
+                    $Uri -eq 'https://api.cloudflare.com/client/v4/zones/12345/dns_records/12345' -and
+                    $Method -eq 'DELETE'
+                }
+            }
+            It 'Calls Invoke-CFRestMethod with correct parameters for ZoneID' {
+                Remove-CFZoneRecord -ZoneID '12345' -RecordId '12345'
+                Assert-MockCalled Invoke-CFRestMethod -Exactly 1 -Scope It -ParameterFilter {
+                    $Uri -eq 'https://api.cloudflare.com/client/v4/zones/12345/dns_records/12345' -and
+                    $Method -eq 'DELETE'
+                }
+            }
+            It 'Returns null' {
+                $result = Remove-CFZoneRecord -ZoneName 'example.com' -RecordId '12345'
+                $result | Should -BeExactly $null
+            }
+        }
+        AfterAll {
+            $script:cfSession | Remove-Variable
+        }
+    }
+}
---
+++ src/Tests/Unit/Public/Test-CloudflareSession.Tests.ps1
@@ -0,0 +1,62 @@
+﻿#-------------------------------------------------------------------------
+Set-Location -Path $PSScriptRoot
+#-------------------------------------------------------------------------
+$ModuleName = 'pwshCloudflare'
+$PathToManifest = [System.IO.Path]::Combine('..', '..', '..', $ModuleName, "$ModuleName.psd1")
+#-------------------------------------------------------------------------
+if (Get-Module -Name $ModuleName -ErrorAction 'SilentlyContinue') {
+    #if the module is already in memory, remove it
+    Remove-Module -Name $ModuleName -Force
+}
+Import-Module $PathToManifest -Force
+#-------------------------------------------------------------------------
+
+InModuleScope 'pwshCloudflare' {
+    Describe 'Test-CloudflareSession Function Tests' -Tag Unit {
+        BeforeAll {
+            $script:cfBaseApiUrl = 'https://api.cloudflare.com/client/v4'
+            Mock Invoke-RestMethod { return [PSCustomObject]@{ success = $true } }
+        }
+
+        Context 'Success' {
+            It 'Successfully tests token authentication' {
+                $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+                $session.Headers.Add('Authorization', 'Bearer validToken')
+                $result = Test-CloudflareSession -Session $session
+                $result.TokenAuthSuccess | Should -Be $true
+            }
+
+            It 'Successfully tests legacy authentication' {
+                $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+                $session.Headers.Add('X-Auth-Email', 'user@example.com')
+                $session.Headers.Add('X-Auth-Key', 'validApiKey')
+                $result = Test-CloudflareSession -Session $session
+                $result.LegacyAuthSuccess | Should -Be $true
+            }
+        }
+
+        Context 'Error' {
+            It 'Fails token authentication with invalid token' {
+                Mock Invoke-RestMethod { throw }
+                $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+                $session.Headers.Add('Authorization', 'Bearer invalidToken')
+                { Test-CloudflareSession -Session $session } | Should -Throw
+            }
+
+            It 'Fails legacy authentication with invalid credentials' {
+                Mock Invoke-RestMethod { throw }
+                $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+                $session.Headers.Add('X-Auth-Email', 'user@example.com')
+                $session.Headers.Add('X-Auth-Key', 'invalidApiKey')
+                { Test-CloudflareSession -Session $session } | Should -Throw
+            }
+
+            It 'Handles missing token and legacy credentials' {
+                $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+                $result = Test-CloudflareSession -Session $session
+                $result.TokenAuthSuccess | Should -Be $false
+                $result.LegacyAuthSuccess | Should -Be $false
+            }
+        }
+    }
+}
