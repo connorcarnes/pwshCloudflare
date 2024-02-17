e70c7559812323a221c5bba4cbcae9469d35cede
connorcarnes <connorcarnes44@gmail.com>
2024-02-17 00:44:07 -0600

    Add test helper Get-MockApiResponse.ps1

---
+++ src/pwshCloudflare/Resources/Get-MockApiResponse.ps1
@@ -0,0 +1,23 @@
+﻿function Get-MockApiResponse {
+    param ()
+    $ResultInfo = [PSCustomObject]@{
+        page        = 1
+        per_page    = 20
+        total_pages = 1
+        count       = 2
+        total_count = 2
+    }
+    $Result = @(
+        [PSCustomObject]@{id = 1; name = 'resourceOne' },
+        [PSCustomObject]@{id = 2; name = 'resourceTwo' },
+        [PSCustomObject]@{id = 3; name = 'resourceThree' }
+    )
+    $Response = [PSCustomObject]@{
+        result      = $Result
+        result_info = $ResultInfo
+        success     = $true
+        errors      = @()
+        messages    = @()
+    }
+    $Response
+}
